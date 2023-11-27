a2548de17e32724528e65b15b127de9de0d5b511
Juyo84 <juliomeme@hotmail.com>
2023-11-27 00:26:32 -0600

    Modificando P9

--- Practica9.py
+++ Practica9.py
@@ -4,14 +4,17 @@ import matplotlib.pyplot as plt
 
 datos = pd.read_csv('Kobe_shots_cleanData.csv')
 
-def prediccion():
+def prediccion_tirosAnotados_Lesion():
 
-    c1 = datos["matchup"] == 'Visitante'
-    c2 = datos["game_date"] > '2013-04-12'
-    c3 = datos["game_date"] < '2016-04-13'
-    c4 = datos["shot_made_flag"] == 1
-    dfGroup = datos.where(c1 & c2 & c3 & c4)
-    db = dfGroup.groupby("game_date").agg({"shot_type": "sum"})
+    datos['game_date'] = pd.to_datetime(datos['game_date']).dt.year
+
+    c1 = datos["shot_made_flag"] == 1
+    c2 = datos["game_date"] >= 2013
+    c3 = datos["game_date"] <= 2016
+
+    dfGroup = datos[c1 & c2 & c3]
+    
+    db = dfGroup.groupby(datos["game_date"]).agg({"shot_type": "sum"})
     db.reset_index(inplace=True)
 
     model = smf.ols('shot_type ~ game_date', db)
@@ -22,10 +25,10 @@ def prediccion():
     plt.scatter(db['game_date'], db['shot_type'])
     plt.plot(db['game_date'], predictions['mean'], color='red')
     plt.fill_between(db['game_date'], predictions['obs_ci_lower'], predictions['obs_ci_upper'], alpha=.1)
+    
     plt.xlabel('Fecha')
     plt.ylabel('Total Puntos')
     plt.title('Prediccion de puntos')
-    plt.show()
     plt.savefig("Graficas_P9/Prediccion_Puntos_Lesion.png")
 
-prediccion()
+prediccion_tirosAnotados_Lesion()
